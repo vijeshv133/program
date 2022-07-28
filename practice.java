ccc5f068598f297e8c359991067d5318a17e6142
vijeshv133 <vijeshv133@gmail.com>
2022-07-28 11:08:51 +0530

    added a program

---
+++ practice.java
@@ -0,0 +1,13 @@
+public class program{
+  public static void main(String arg[])
+   { int a =10;
+      int b=20;
+if (a<b)
+    {system.out.println("a is less");
+  }
+else
+  { system.out.println("b is less");
+}
+ 
+}
+}
